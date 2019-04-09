72dcf89dcde2696c0d204d71381aee7c5526cba5
Synara Soares <synara-s@hotmail.com>
2019-04-09 00:33:22 -0300

    ajustes no menu

--- principal.py
+++ principal.py
@@ -3,15 +3,17 @@ from user import User
 
 def menu():
     print("---------- MENU ----------")
-    print("1. generate data \n2. find user\n3. show users")  
+    print("1. generate data \n2. constains user\n3. find user\n4. show users")  
     return int(input("choose one of the options: "))
 
-def options(option, tablesize, hashtable):
-    generates = False
+
+def alert():
+    print("there's not users on the base. firstly, generate data.")
     
       
 if __name__ == '__main__':
     
+    generates = False
     option = menu()
     tablesize = 0
     hashtable = HashingTable(tablesize)
@@ -21,21 +23,31 @@ if __name__ == '__main__':
         hashtable = HashingTable(tablesize)
         hashtable.generatedata()
         generates = True
-        hashtable.showusers()
     
-    elif option == 2:
+    elif  option == 2:
+        if generates:
+            key = input("enter the user name: ")
+            exists = hashtable.contains(key)
+        
+            if exists:
+                print("the requested user exists in the database.")
+            else: 
+                print("the informed user doesn't exist in the database.")
+        else: 
+            alert()
+
+    elif option == 3:
         if generates:
             key = input("enter the user name: ")
             hashtable.get(key)
         else: 
-            print("there's not users on the base. firstly, generate data.")
-    
-    elif option == 3:
+            alert()
+
+    elif option == 4:
         if generates: 
             hashtable.showusers()
         else: 
-            print("no data has been generated, so there are no users to view.")
-    print("\n\n")
+            alert()
     
     while True:
         n = input('continue? [y/n] ')
@@ -46,19 +58,30 @@ if __name__ == '__main__':
                 hashtable = HashingTable(tablesize)
                 hashtable.generatedata()
                 generates = True
-                hashtable.showusers()
+        
             elif option == 2:
+                if generates:
+                    key = input("enter the user name: ")
+                    exists = hashtable.contains(key)
+                    if exists:
+                        print("the requested user exists in the database.")
+                    else: 
+                        print("the informed user doesn't exist in the database.")
+                else: 
+                    alert()
+
+            elif option == 3:
                 if generates:
                     key = input("enter the user name: ")
                     hashtable.get(key)
                 else: 
-                    print("there's not users on the base. firstly, generate data.")
-    
-            elif option == 3:
+                    alert()
+
+            elif option == 4:
                 if generates: 
                     hashtable.showusers()
                 else: 
-                    print("no data has been generated, so there are no users to view.")
+                    alert()
         else:
             exit()
 
